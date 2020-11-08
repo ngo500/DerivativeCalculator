1bbed0024ac2ea4122e87ed3557e1cde34ac1396
gregchen56 <72785969+gregchen56@users.noreply.github.com>
2020-11-07 23:19:46 -0800

    Update ButtonHandler.java

--- JavaFX GUI/src/application/ButtonHandler.java
+++ JavaFX GUI/src/application/ButtonHandler.java
@@ -4,28 +4,13 @@ import javafx.scene.control.Button;
 
 public class ButtonHandler
 {
-	public ButtonHandler()
-	{
-		
-	}
-	
-	
-	public Button create60pxButton(String buttonText)
+	// Creates a button with the provided text, width, and height
+	public Button createButton(String buttonText, int width, int height)
 	{
 		Button Btn = new Button(buttonText);
-		Btn.setPrefWidth(60);
+		Btn.setPrefWidth(width);
+		Btn.setPrefHeight(height);
 		
 		return Btn;
 	}
-	
-	
-	public Button createCalculateButton()
-	{
-		Button calculate = new Button("Calculate");
-		calculate.setPrefHeight(50);
-		calculate.setPrefWidth(110);
-		
-		return calculate;
-	}
-	
 }
