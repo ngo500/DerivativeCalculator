21a54c5bab63df66f33ec02968a7ea11fadc867f
Andrew Tedijanto <andrew.tedijanto@gmail.com>
2020-11-30 21:33:12 -0800

    Added SqrtFunction

---
+++ JavaFX GUI/src/DerivativeFunction/SqrtFunction.java
@@ -0,0 +1,17 @@
+package DerivativeFunction;
+
+public class SqrtFunction implements DerivativeFunction {
+
+	@Override
+	public int displace() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	@Override
+	public String returnDerivative() {
+		// TODO Auto-generated method stub
+		return "(1/2)^(-1/2)";
+	}
+
+}
