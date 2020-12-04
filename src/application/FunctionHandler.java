27bd2eff5bc839f54f82fbdcb054cb66d51d3822
Andrew Tedijanto <andrew.tedijanto@gmail.com>
2020-12-04 12:48:29 -0800

    Removed comment from FunctionHandler

--- src/application/FunctionHandler.java
+++ src/application/FunctionHandler.java
@@ -11,13 +11,11 @@ import derivativefunction.NaturalLogFunction;
 import derivativefunction.ExponentialFunction;
 import derivativefunction.SqrtFunction;
 
-public class FunctionHandler {
+/**
+ * Encapsulates all derivative functions for class use
+ */
 
-	/**
-	 * Function that takes the given input and applies a function to it in order to get the derivative
-	 * @param term - the String given from the parsed input
-	 * @return result - the result of the derived term
-	 */
+public class FunctionHandler {
 	
 	public String sinDeriv() { return new SinFunction().returnDerivative(); }
 	public int sinDisplace() { return new SinFunction().displace(); }
@@ -48,96 +46,5 @@ public class FunctionHandler {
 	
 	public String sqrtDeriv() { return new SqrtFunction().returnDerivative(); }
 	public int sqrtDisplace() { return new SqrtFunction().displace(); }
-	
-	/*
-	public String derive(String term) {
-		if(term.equals("+") || term.equals("-"))
-			return term;
-		
-		//result of derivative to return
-		String result = "";
-		
-		if(term.equals("")) {
-			
-			//empty input
-			result = "";
-			
-		}//if
-		else if(term.contains("sin")) {
-			
-			//call sin function
-			SinFunction sf = new SinFunction();
-			result = sf.returnDerivative();
-			
-		}//else if
-		else if(term.contains("cos")) {
-			
-			//call cos function
-			CosFunction cf = new CosFunction();
-			result = cf.returnDerivative();
-			
-		}//else if
-		else if(term.contains("tan")) {
-			
-			//call tan function
-			TanFunction tf = new TanFunction();
-			result = tf.returnDerivative();
-			
-		}//else if
-		else if(term.contains("sxn")) {
-			
-			//call inv sin function
-			InvSinFunction isf = new InvSinFunction();
-			result = isf.returnDerivative();
-			
-		}//else if
-		else if(term.contains("cxs")) {
-			
-			//call inv cos function
-			InvCosFunction icf = new InvCosFunction();
-			result = icf.returnDerivative();
-			
-		}//else if
-		else if(term.contains("txn")) {
-			
-			//call inv tan function
-			InvTanFunction itf = new InvTanFunction();
-			result = itf.returnDerivative();
-			
-		}//else if
-		else if(term.contains("e")) {
-			
-			//call e function
-			EFunction ef = new EFunction();
-			result = ef.returnDerivative();
-			
-		}//else if
-		else if(term.contains("ln")) {
-			
-			//call ln function
-			NaturalLogFunction lnf = new NaturalLogFunction();
-			result = lnf.returnDerivative();
-			
-		}//else if
-		else if(term.contains("sqrt")) {
-			
-			//call square root function
-			SqrtFunction sqrtf = new SqrtFunction();
-			result = sqrtf.returnDerivative();
-			
-		}//else if
-		else {
-			
-			//probably is just an x or a number
-			PowerRule pr = new PowerRule();
-			result = pr.calculateRule(term);
-			
-		}//else
-		
-		//result derived to return
-		return result;
-		
-	}//derive
-	*/
 	
 }
