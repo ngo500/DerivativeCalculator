66edb5cebdea49fd308bc03c2166ecde08dfb339
ngo500 <72844093+ngo500@users.noreply.github.com>
2020-12-05 16:15:55 -0800

    Update ChainRule.java
    added check for negative terms to put () around

--- ChainRule.java
+++ ChainRule.java
@@ -1,4 +1,4 @@
-package DerivativeFunction;
+package derivativerule;
 
 /**
  * 
@@ -9,7 +9,9 @@ package DerivativeFunction;
  *
  */
 
-public class ChainRule implements DerivativeRule{
+import application.FunctionHandler;
+
+public class ChainRule implements DerivativeRule {
 
 	@Override
 	public String calculateRule(String st) {
@@ -21,8 +23,10 @@ public class ChainRule implements DerivativeRule{
 	 * @param String out is for the outside function, String in is for the inside function
 	 * @return String is the derivative resulting from the chain function
 	 */
-	public String calculateRule(String out, String in) {
+	public String calculateRule(String out, String in) throws NumberFormatException {
 		
+		FunctionHandler fh = new FunctionHandler();
+
 		//string out = fx, string in = gx
 		//result from rule
 		String result = "";
@@ -50,63 +54,185 @@ public class ChainRule implements DerivativeRule{
 		}//if
 		else {	//otherwise there is only 1 term, just use it as is
 			PowerRule pf = new PowerRule();
+			
 			result = ("(" + pf.calculateRule(in) + ")");
+			if(result.equals("(1)"))
+				result = "";
+			else if(result.equals("(0)"))
+				return "0";
 		}//else
 		
-		//now we have (gx')
 		
+		//now we have (gx')
+		String outoutside = "";
+ 		int i = 0;
+ 		if(!out.matches("[0-9]+")) {
+ 			while(Character.isDigit(out.charAt(i)) || out.charAt(i) == 'i' || out.charAt(i) == 'p') {
+ 				if(out.charAt(i) == 'p') {
+ 					if(!out.contains("pi"))
+ 						throw new NumberFormatException();
+ 				}
+ 				outoutside += Character.toString(out.charAt(i));
+ 				i++;
+ 				if(i == out.length())
+ 					break;
+ 			}
+ 			out = out.substring(i);
+ 		}
+ 		int addedLength = result.length();
 		
 		//next term- fx'
-		if(out.equals("sin")) {	//function is sin
+		int displace = 0;
+		if(out.equals("sin^-1")) { //function is sin^-1
 			
-			SinFunction sf = new SinFunction();
-			result += sf.returnDerivative();
+			if(!outoutside.equals("")) {
+ 				result += '(';
+ 				result += fh.invSinDeriv();
+ 				result += ')';
+ 				displace = fh.invSinDisplace() + 1;
+ 			}
+ 			else {
+ 				result += fh.invSinDeriv();
+ 				displace = fh.invSinDisplace();
+ 			};
 			
-		
 		}//if
-		else if(out.equals("cos")) { //function is cos
-			
-			CosFunction sf = new CosFunction();
-			result += sf.returnDerivative();
-			
-		}//else if
-		else if(out.equals("tan")) { //function is tan
-			
-			TanFunction sf = new TanFunction();
-			result += sf.returnDerivative();
-			
-		}//else if
-		else if(out.equals("sin^-1")) { //function is sin^-1
-			
-			InvSinFunction sf = new InvSinFunction();
-			result += sf.returnDerivative();
-			
-		}//else if
 		else if(out.equals("cos^-1")) { //function is cos^-1
 			
-			InvCosFunction sf = new InvCosFunction();
-			result += sf.returnDerivative();
+			if(!outoutside.equals("")) {
+ 				result += '(';
+ 				result += fh.invCosDeriv();
+ 				result += ')';
+ 				displace = fh.invCosDisplace() + 1;
+ 			}
+ 			else {
+ 				result += fh.invCosDeriv();
+ 				displace = fh.invCosDisplace();
+ 			}
 			
 		}//else if
 		else if(out.equals("tan^-1")) { //function is tan^-1
 			
-			InvTanFunction sf = new InvTanFunction();
-			result += sf.returnDerivative();
+			if(!outoutside.equals("")) {
+ 				result += '(';
+ 				result += fh.invTanDeriv();
+ 				result += ')';
+ 				displace = fh.invTanDisplace() + 1;
+ 			}
+ 			else {
+ 				result += fh.invTanDeriv();
+ 				displace = fh.invTanDisplace();
+ 			}
 			
 		}//else if
+		else if(out.equals("sin")) {	//function is sin
+
+ 			if(!outoutside.equals("")) {
+ 				result += '(';
+ 				result += fh.sinDeriv();
+ 				result += ')';
+ 				displace = fh.sinDisplace() + 1;
+ 			}
+ 			else {
+ 				result += fh.sinDeriv();
+ 				displace = fh.sinDisplace();
+ 			}
+
+		
+		}//else if
+		else if(out.equals("cos")) { //function is cos
+			
+			if(!outoutside.equals("")) {
+ 				result += '(';
+ 				result += fh.cosDeriv();
+ 				result += ')';
+ 				displace = fh.cosDisplace() + 1;
+ 			}
+ 			else {
+ 				result += fh.cosDeriv();
+ 				displace = fh.cosDisplace();
+ 			}
+
+			
+		}//else if
+		else if(out.equals("tan")) { //function is tan
+			
+			if(!outoutside.equals("")) {
+ 				result += '(';
+ 				result += fh.tanDeriv();
+ 				result += ')';
+ 				displace = fh.tanDisplace() + 1;
+ 			}
+ 			else {
+ 				result += fh.tanDeriv();
+ 				displace = fh.tanDisplace();
+ 			}
+			
+		}//else if
+		else if(out.contains("ln")) { //function is ln
+			
+			if(!outoutside.equals("")) {
+ 				result += '(';
+ 				result += fh.lnDeriv();
+ 				result += ')';
+ 				displace = fh.lnDisplace() + 1;
+ 			}
+ 			else {
+ 				result += fh.lnDeriv();
+ 				displace = fh.lnDisplace();
+ 			}
+			
+		}//else if
+		else if(out.contains("sqrt")) { //function is sqrt
+			result += fh.sqrtDeriv();
+			displace = fh.sqrtDisplace();
+		}
+		else if(out.contains("^")) {
+			if(out.contains("e^")) {
+				// make e function
+				result += fh.eDeriv();
+				displace = fh.eDisplace();
+			}
+			else {
+				if(out.matches("[0-9]+(\\^)") || out.matches("[0-9]*[e]?(pi\\^)")) {
+					String base = out.substring(0, out.length() - 1);
+					// make base function
+					result += fh.expDeriv(base);
+					displace = fh.expDisplace(base);
+				}
+				else
+					throw new NumberFormatException();
+			}
+		}
 		else {
 			
 			PowerRule pf = new PowerRule();
 			result += pf.calculateRule(in);
 			
 		}//else
-		
 		//now we have gx'fx'(missing gx)
-		
 		//add in gx into the last spot in parenthesis
-		result += ("(" + in + ")");
+		if(displace == 0)
+			result += ("(" + in + ")");
+		else
+			result = result.substring(0, displace + addedLength) + in + result.substring(displace + addedLength);
+		
+		outoutside = outoutside + result;
+		result = outoutside;
+		
+		//check for negative terms to put ( ) around
+		if(result.contains("-") && result.charAt(0) != '-') {
+			int index = result.indexOf('-') + 1;
+			if(result.charAt(index) == 's' || result.charAt(index) == 'c' || result.charAt(index) == 't') {
+				String delimiter2 = "((?<=\\-)|(?=\\-))";
+				String[] arr2 = result.split(delimiter2);	//split for easy parsing
+				result = arr2[0] + "(" + arr2[1] + arr2[2] + ")";
+			}//if
+		}//if
+		else {}//else
 		
 		return result;	//return gx' fx'(gx)
+		
 	}//calculateRule	
 
 }//ChainRule
